60a51d978bb5b4f06c60a43b4a4dcb545f366729
Lokesh Vishwakarma <uvishwakarma3@gmail.com>
2020-09-17 20:14:59 +0530

    Implemented Vector class test cases

--- test/test.py
+++ test/test.py
@@ -13,8 +13,26 @@ class TestVectors(unittest.TestCase):
     def test_addition(self):
         _sum = self.v1 + self.v2
         self.assertEqual(getattr(_sum, 'x'), 4.0)
-        self.assertEqual(getattr(_sum, 'y'), -8.0)
-        self.assertEqual(getattr(_sum, 'z'), -11.0)
+        self.assertEqual(getattr(_sum, 'y'), 4.0)
+        self.assertEqual(getattr(_sum, 'z'), 7.0)
+
+    def test_subtraction(self):
+        _diff = self.v1 - self.v2
+        self.assertEqual(getattr(_diff, 'x'), -2.0)
+        self.assertEqual(getattr(_diff, 'y'), -8.0)
+        self.assertEqual(getattr(_diff, 'z'), -11.0)
+
+    def test_multiplication(self):
+        _mul = self.v1 * 2
+        self.assertEqual(getattr(_mul, 'x'), 2.0)
+        self.assertEqual(getattr(_mul, 'y'), -4.0)
+        self.assertEqual(getattr(_mul, 'z'), -4.0)
+
+    def test_division(self):
+        _div = self.v1 / self.v2
+        self.assertEqual(getattr(_div, 'x'), 2.0)
+        self.assertEqual(getattr(_div, 'y'), -4.0)
+        self.assertEqual(getattr(_div, 'z'), -4.0)
 
 
 if __name__ == '__main__':
